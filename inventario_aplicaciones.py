6829cc1197ef0747e24e9c558f80125486fa5ef9
Ricardo Nuñez <Ricardo.Nunez@grupocepas.com>
2022-09-25 16:31:04 -0300

    Agrego Modificar con la gráfica que hizo Pamela y unos retoques

--- inventario_aplicaciones.py
+++ inventario_aplicaciones.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
@@ -55,14 +56,21 @@ def valid_int(nombre_campo: str, cadena: str) -> str:
         return ''
 
 
-def alta(nombre: str, tipo: str, nivel, ruta: str, descripcion: str, tree):
+def alta(id, nombre: str, tipo: str, nivel, ruta: str, descripcion: str, tree):
     errores: str = ''
     errores += valid_string('Nombre de la Aplicación', nombre)
     errores += valid_int('Nivel de Riesgo', nivel)
     if errores == '':
         #Hacer el Alta o la Modificación
         if boton_alta['text'] == "Actualizar":
-            messagebox.showinfo("Modificación", "hacer la modificacion a la base de datos")
+            con = conexion()
+            cursor = con.cursor()
+            data = ( nombre, tipo, nivel, ruta, descripcion, id)
+            sql = "UPDATE aplicaciones SET nombre = ?, tipo = ? , nivel = ?, ruta = ?, descripcion = ? WHERE id = ?"
+            cursor.execute(sql, data)
+            con.commit()
+            messagebox.showinfo("Modificación", "Se actualizó exitosamente")
+            actualizar_treeview(tree)
         else:
             con = conexion()
             cursor = con.cursor()
@@ -140,72 +148,102 @@ def buscar_una_app( id ):
 # VISTA
 # ##############################################
 
+#Defino ventana principal de la Aplicación
 root = Tk()
-root.title("Tarea POO")
 
-titulo = Label(root, text="Ingrese los datos de la aplicación", bg="#76B6E3", fg="White", height=1, width=60)
-titulo.grid(row=0, column=0, columnspan=6, padx=1, pady=1, sticky=W + E)
+#Título, tamaño y color de la ventana principal
+root.title("Inventario de Aplicaciones")
+root.geometry("560x485")
+root.config(bg="#FFFDFD")
 
-nombre = Label(root, text="ID")
-nombre.grid(row=1, column=0, sticky=W)
-nombre = Label(root, text="Nombre")
-nombre.grid(row=2, column=0, sticky=W)
-tipo = Label(root, text="Tipo")
-tipo.grid(row=3, column=0, sticky=W)
-nivel = Label(root, text="Nivel de Riesgo")
-nivel.grid(row=4, column=0, sticky=W)
-ruta = Label(root, text="Ruta del Log")
-ruta.grid(row=5, column=0, sticky=W)
-descripcion = Label(root, text="Descripción")
-descripcion.grid(row=6, column=0, sticky=W)
+#Título del cuadro principal
+titulo = Label(root, text="Ingrese los datos de la Aplicación", bg="#76B6E3", fg="White", height=1, width=80)
+#Título del cuadro detalle
+titulo_detalle = Label(root, text="Listado de Aplicaciones", bg="#76B6E3", fg="White", height=1, width=80)
 
 # Defino variables para tomar valores de campos de entrada
 id_val, nombre_val, tipo_val, nivel_val, ruta_val, descripcion_val = IntVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar()
-w_ancho = 20
+w_ancho = 50
 
-entrada_id = Entry(root, textvariable=id_val, state="readonly", width=w_ancho)
-entrada_id.grid(row=1, column=1)
+# Etiquetas / Labels:
+id = Label(root, text="ID",bg="#FFFDFD")
+nombre = Label(root, text="Nombre",bg="#FFFDFD")
+tipo = Label(root, text="Tipo",bg="#FFFDFD")
+nivel = Label(root, text="Nivel de Riesgo",bg="#FFFDFD")
+ruta = Label(root, text="Ruta del Log",bg="#FFFDFD")
+descripcion = Label(root, text="Descripción",bg="#FFFDFD")
+
+# Campos de ingreso de información / Entries:
+entrada_id = Entry(root, textvariable=id_val, state="readonly", width=10)
 entrada_nombre = Entry(root, textvariable=nombre_val, width=w_ancho)
-entrada_nombre.grid(row=2, column=1)
 entrada_tipo = Entry(root, textvariable=tipo_val, width=w_ancho)
-entrada_tipo.grid(row=3, column=1)
 entrada_nivel = Entry(root, textvariable=nivel_val, width=w_ancho)
-entrada_nivel.grid(row=4, column=1)
 entrada_ruta = Entry(root, textvariable=ruta_val, width=w_ancho)
-entrada_ruta.grid(row=5, column=1)
 entrada_descripcion = Entry(root, textvariable=descripcion_val, width=w_ancho)
-entrada_descripcion.grid(row=6, column=1)
+
+# Botones / Buttons:
+boton_alta = Button(
+    root,
+    text="Agregar",
+    command=lambda: alta(
+        id_val.get(),
+        nombre_val.get(),
+        tipo_val.get(),
+        nivel_val.get(),
+        ruta_val.get(),
+        descripcion_val.get(),
+        tree)
+    )
+boton_consulta = Button(root, text="Consultar", command=lambda: consultar(tree))
+boton_borrar = Button(root, text="Borrar", command=lambda: borrar(tree))
+boton_modificar = Button(root, text="Modificar", command=lambda: modificar(tree))
+boton_salir = Button(root, text="Salir", command=root.quit)
 
 # --------------------------------------------------
 # TREEVIEW
 # --------------------------------------------------
 
 tree = ttk.Treeview(root)
+
+# Posición de etiquetas, campos de entrada y botones /Labels, Entries, Buttons
+#Título
+titulo.grid(row=0, column=0, columnspan=6, padx=1, pady=1, sticky=W + E)
+
+#Labels
+id.grid(row=1, column=1)
+nombre.grid(row=2, column=1, sticky=E)
+tipo.grid(row=3, column=1, sticky=E)
+nivel.grid(row=4, column=1, sticky=E)
+ruta.grid(row=5, column=1, sticky=E)
+descripcion.grid(row=6, column=1, sticky=E)
+#Entries
+entrada_id.grid(row=1, column=2)
+entrada_nombre.grid(row=2, column=2)
+entrada_tipo.grid(row=3, column=2)
+entrada_nivel.grid(row=4, column=2)
+entrada_ruta.grid(row=5, column=2)
+entrada_descripcion.grid(row=6, column=2)
+#Buttons
+boton_alta.grid(row=7, column=2, sticky=EW)
+
+#Título Detalle
+titulo_detalle.grid(row=8, column=0, columnspan=6, padx=1, pady=1, sticky=W + E)
+boton_consulta.grid(row=9, columnspan=1, column=1, sticky=EW)
+
+#Árbol de información
+tree.grid(row=10, column=1, columnspan=4)
 tree["columns"] = ("col1", "col2", "col3")
 tree.column("#0", width=90, minwidth=50, anchor=W)
 tree.column("col1", width=100, minwidth=80)
 tree.column("col2", width=50, minwidth=80)
-tree.column("col3", width=150, minwidth=80)
+tree.column("col3", width=250, minwidth=80)
 tree.heading("#0", text="ID")
-tree.heading("col1", text="nombre")
-tree.heading("col2", text="tipo")
+tree.heading("col1", text="Nombre")
+tree.heading("col2", text="Tipo")
 tree.heading("col3", text="Descripción")
 
-tree.grid(row=11, column=0, columnspan=4)
-
-boton_alta = Button(root, text="Alta",
-                    command=lambda: alta(nombre_val.get(), tipo_val.get(), nivel_val.get(),
-                                         ruta_val.get(), descripcion_val.get(), tree)
-                    )
-boton_alta.grid(row=7, column=1)
-
-boton_consulta = Button(root, text="Consultar", command=lambda: consultar(tree))
-boton_consulta.grid(row=8, column=1)
-
-boton_borrar = Button(root, text="Borrar", command=lambda: borrar(tree))
-boton_borrar.grid(row=9, column=1)
-
-boton_modificar = Button(root, text="Modificar", command=lambda: modificar(tree) )
-boton_modificar.grid(row=10, column=1)
+boton_borrar.grid(row=15, column=1, sticky=EW)
+boton_modificar.grid(row=15, column=2, sticky=EW)
+boton_salir.grid(row=15, column=4, sticky=EW)
 
 root.mainloop()
